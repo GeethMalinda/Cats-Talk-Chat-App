01b575d871507f80a33f4658827c12d80c8815e2
GeethMalinda <kdgmwijesekara22@gmail.com>
2021-09-19 12:14:32 +0530

    image upload ,npm outadated ,firestorage

--- Screens/PostScreen.js
+++ Screens/PostScreen.js
@@ -1,29 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Platform, StyleSheet, View, } from 'react-native'
+import React, { useState } from 'react'
+import { AddImage, InputField, InputWrapper } from '../Styles/AddPost'
+import ActionButton from 'react-native-action-button'
+import Icon from 'react-native-vector-icons/Ionicons'
+import ImagePicker from 'react-native-image-crop-picker'
 
 const PostScreen = () => {
 
+  const [image, setImage] = useState(null)
+  const [uploading, setUploading] = useState(false)
+  const [transferred, setTransferred] = useState(0)
+  const [post, setPost] = useState(null)
+
+  const takePhotoFromCamera = () => {
+    ImagePicker.openCamera({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then((image) => {
+      console.log(image)
+      const imageUri = Platform.OS === 'ios' ? image.sourceURL : image.path
+      setImage(imageUri)
+    })
+  }
+
+  const choosePhotoFromLibrary = () => {
+    ImagePicker.openPicker({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then((image) => {
+      console.log(image)
+      const imageUri = Platform.OS === 'ios' ? image.sourceURL : image.path
+      setImage(imageUri)
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome Post Screen</Text>
-
+      <InputWrapper>
+        {image != null ? <AddImage source={{ uri: image }}/> : null}
+        <InputField
+          placeholder="Whats On Your Mind "
+          multiline
+          numberOfLines={6}/>
+      </InputWrapper>
+      <ActionButton buttonColor="#2e64e5">
+        <ActionButton.Item
+          buttonColor="#9b59b6"
+          title="Take Photo"
+          onPress={takePhotoFromCamera}>
+          <Icon name="camera-outline" style={styles.actionButtonIcon}/>
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#3498db"
+          title="Choose Photo"
+          onPress={choosePhotoFromLibrary}>
+          <Icon name="md-images-outline" style={styles.actionButtonIcon}/>
+        </ActionButton.Item>
+      </ActionButton>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f9fafd',
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
+    justifyContent: 'center',
   },
-  text: {
-    fontFamily: 'Kufam-SemiBoldItalic',
-    fontSize: 28,
-    color: '#3333',
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
   },
 })
-
 export default PostScreen
